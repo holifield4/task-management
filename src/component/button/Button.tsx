029c477714201c3a83acb05aaa8c57cc3f00a797
holifieldw <holifield04@gmail.com>
2024-12-11 21:04:36 +0800

    feat: add button component

---
+++ src/component/button/Button.tsx
@@ -0,0 +1,26 @@
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    label: string;
+    color?: "primary" | "neutral" | "danger";
+}
+
+export default function Button({ color = "primary", ...props }: ButtonProps){
+
+    const colorClasses = {
+        primary: "bg-blue-600 hover:bg-blue-700",
+        danger: "bg-red-600 hover:bg-red-700",
+        neutral: "bg-green-600 hover:bg-green-700",
+      };
+
+    return(
+        <>
+            <button
+                form={props.form}
+                onClick={props.onClick}
+                disabled={props.disabled}
+                className={`px-4 h-8 rounded-md ${colorClasses[color]} text-xs tracking-wider text-white transition-colors`}
+            >
+                {props.label}
+            </button>
+        </>
+    )
+}
