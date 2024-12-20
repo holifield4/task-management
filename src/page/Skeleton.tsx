450c444ad3d225603e29394d049e5c19c9893e88
holifieldw <holifield04@gmail.com>
2024-12-20 18:38:49 +0800

    KIV: temporary skeleton page

---
+++ src/page/Skeleton.tsx
@@ -0,0 +1,7 @@
+export default function Skeleton(){
+    return (
+        <div className="w-full h-[calc(100vh-88px) flex items-center justify-center">
+            <h1>Loading...</h1>
+        </div>
+    )
+}
