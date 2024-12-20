f0da9b8b2c08af8a3f7e8c82b06a4f184a017087
holifieldw <holifield04@gmail.com>
2024-12-20 18:40:01 +0800

    feat: WIP homepage development

--- src/page/Homepage.tsx
+++ src/page/Homepage.tsx
@@ -1,27 +1,44 @@
 import Button from "../component/button/Button";
 import Section from "../component/section/Section";
 import Select from "../component/select/Select";
+import DataTable, { Column } from "../component/table/DataTable";
 import TextField from "../component/textfield/TextField";
-import { IDropdown } from "../interface/common";
+import { ITask } from "../interface/common";
+import { taskStatusOptions } from "../lib/constant/constant";
+import { taskData } from "../mocks/mockdata";
 
 export default function Homepage() {
-  const options: IDropdown[] = [
-    { name: "Done", value: "Done" },
-    { name: "In Progress", value: "In Progress" },
-    { name: "Pending", value: "Pending" },
+  const columns: Column<ITask>[] = [
+    { key: "id", header: "ID" },
+    { key: "name", header: "Name" },
+    {
+      key: "status",
+      header: "Status",
+      render: (value) => (
+        <span className="rounded-full bg-gray-100 px-2 py-1 text-sm">
+          {value}
+        </span>
+      ),
+    },
+    { key: "parentId", header: "Parent ID" },
   ];
+
   return (
     <>
-      <div className="h-[calc(100vh-48px)] p-2 overflow-auto bg-slate-200">
+      <div className="h-[calc(100vh-88px)] p-2 overflow-auto bg-slate-50">
         <Section title="Section">
           <TextField label="Task Name" />
-          <TextField label="Task Name" />
-          <TextField label="Task Name" />
-          <Select label="Status" options={options} />
+          <TextField label="Task Name" errors />
+          <Select label="Status" options={taskStatusOptions} errors />
+          <Select label="Status" options={taskStatusOptions} />
           <Button label="Click Here" />
           <Button label="Click Here" color="neutral" />
           <Button label="Click Here" color="danger" />
         </Section>
+
+        <div className="w-full h-[300px] overflow-y-auto">
+          <DataTable data={taskData} columns={columns} />
+        </div>
       </div>
     </>
   );
