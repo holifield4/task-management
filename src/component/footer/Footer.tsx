34815c51b9c0ffba2277bf6167432e31809eb3e1
holifieldw <holifield04@gmail.com>
2024-12-20 18:37:50 +0800

    feat: add footer

---
+++ src/component/footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <footer className="w-full h-10 col-span-7 border-t border-slate-300 bg-slate-100 flex items-center justify-center text-xs">
+            <p><span className="font-semibold">Aphrodite&#8482;</span> Copyright 2024. All right reserved. holifield04@gmail.com</p>
+        </footer>
+    )
+}
