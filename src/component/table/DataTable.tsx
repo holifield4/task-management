35aa229a1434ade45ae72f6c3e0a728a7d11aee1
holifieldw <holifield04@gmail.com>
2024-12-20 23:41:51 +0800

    feat: WIP modify table component

--- src/component/table/DataTable.tsx
+++ src/component/table/DataTable.tsx
@@ -1,4 +1,6 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
+import Button from "../button/Button";
+import TextField from "../textfield/TextField";
 
 export type Column<T> = {
   key: keyof T;
@@ -9,39 +11,47 @@ export type Column<T> = {
 type DataTableProps<T> = {
   data: T[];
   columns: Column<T>[];
+  enableAction?: boolean;
 };
 
 export default function DataTable<T extends { id: number | string }>({
   data,
   columns,
+  enableAction,
 }: DataTableProps<T>) {
   return (
-    <div className="overflow-x-auto">
-      <table className="min-w-full border-collapse border border-gray-200">
-        <TableHeader columns={columns} />
+    <div className="overflow-x-auto text-xs h-full">
+      <TableControl />
+      <table className="min-w-full border-collapse rounded-md">
+        <TableHeader columns={columns} actions={enableAction}/>
         <tbody>
           {data.map((row) => (
-            <TableRow key={row.id} row={row} columns={columns} />
+            <TableRow key={row.id} row={row} columns={columns} actions={enableAction}/>
           ))}
         </tbody>
       </table>
+      <TableFooter />
     </div>
   );
 }
 
 type TableHeaderProps<T> = {
   columns: Column<T>[];
+  actions?: boolean;
 };
 
-function TableHeader<T>({ columns }: TableHeaderProps<T>) {
+function TableHeader<T>({ columns, actions }: TableHeaderProps<T>) {
   return (
     <thead>
-      <tr className="bg-gray-100">
+      <tr className="bg-gray-100 sticky top-16">
         {columns.map((column) => (
-          <th key={String(column.key)} className="border p-2">
-            {column.header}
+          <th key={String(column.key)} className="border-x border-b p-2">
+            <Button variant="table" label={column.header} icon="sort" />
           </th>
         ))}
+        {actions && (
+          <th>More</th>
+        )}
       </tr>
     </thead>
   );
@@ -50,11 +60,13 @@ function TableHeader<T>({ columns }: TableHeaderProps<T>) {
 type TableRowProps<T> = {
   row: T;
   columns: Column<T>[];
+  actions?: boolean;
 };
 
 function TableRow<T extends { id: number | string }>({
   row,
   columns,
+  actions
 }: TableRowProps<T>) {
   return (
     <tr key={row.id}>
@@ -65,6 +77,11 @@ function TableRow<T extends { id: number | string }>({
           render={column.render}
         />
       ))}
+      {actions && (
+        <td className="border-y p-2 flex justify-center">
+          <Button label="" variant="table" icon="dots"/>
+        </td>
+      )}
     </tr>
   );
 }
@@ -76,6 +93,36 @@ type TableCellProps<T> = {
 
 function TableCell<T>({ value, render }: TableCellProps<T>) {
   return (
-    <td className="border p-2">{render ? render(value) : value ? String(value) : "Not Available"}</td>
+    <td className="border p-2">
+      {render ? render(value) : value ? String(value) : "Not Available"}
+    </td>
+  );
+}
+
+function TableControl() {
+  return (
+    <div className="w-full h-16 px-4 bg-gray-100 border rounded-t-md border-gray-200 flex justify-between items-center sticky top-0">
+      <div className="w-1/3">
+        <TextField label="" placeholder="Search..." />
+      </div>
+      <div className="w-fit flex gap-3">
+        <Button color="neutral" label="Filter" onClick={() => alert("shesh")} />
+        <Button color="neutral" label="View" icon="adjustment"/>
+        <Button color="neutral" label="" icon="printer"/>
+        <Button color="neutral" label="" icon="pdf"/>
+      </div>
+    </div>
+  );
+}
+
+function TableFooter() {
+  return (
+    <div className="w-full h-12 px-4 bg-gray-100 border rounded-b-md border-gray-200 flex justify-end items-center gap-2.5 sticky bottom-0">
+      <Button color="neutral" label="" icon="chevronDoubleLeft"/>
+      <Button color="neutral" label="" icon="chevronLeft"/>
+      <span>5/10</span>
+      <Button color="neutral" label="" icon="chevronRight"/>
+      <Button color="neutral" label="" icon="chevronDoubleRight"/>
+    </div>
   );
 }
