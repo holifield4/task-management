cae417498f101614228b730cb33c36e7ef99c0da
holifieldw <holifield04@gmail.com>
2024-12-20 18:39:09 +0800

    feat: add constant and mockdata

---
+++ src/lib/constant/constant.ts
@@ -0,0 +1,7 @@
+import { IDropdown } from "../../interface/common";
+
+ export const taskStatusOptions: IDropdown[] = [
+    { name: "Done", value: "Done" },
+    { name: "In Progress", value: "In Progress" },
+    { name: "Pending", value: "Pending" },
+  ];
---
+++ src/mocks/mockdata.ts
@@ -0,0 +1,20 @@
+import { ITask } from "../interface/common";
+
+export const taskData: ITask[] = [
+  {id: 1, name: 'Shower', status: 'Pending'},
+  {id: 2, name: 'Dinner', status: 'Pending'},
+  {id: 1, name: 'Shower', status: 'Pending'},
+  {id: 2, name: 'Dinner', status: 'Pending'},
+  {id: 1, name: 'Shower', status: 'Pending'},
+  {id: 2, name: 'Dinner', status: 'Pending'},
+  {id: 1, name: 'Shower', status: 'Pending'},
+  {id: 2, name: 'Dinner', status: 'Pending'},
+  {id: 1, name: 'Shower', status: 'Pending'},
+  {id: 2, name: 'Dinner', status: 'Pending'},
+  {id: 1, name: 'Shower', status: 'Pending'},
+  {id: 2, name: 'Dinner', status: 'Pending'},
+  {id: 1, name: 'Shower', status: 'Pending'},
+  {id: 2, name: 'Dinner', status: 'Pending'},
+  {id: 1, name: 'Shower', status: 'Pending'},
+  {id: 2, name: 'Dinner', status: 'Pending'},
+]
