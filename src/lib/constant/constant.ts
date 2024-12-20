d3e5649142daf965364e73b78f1317b49b0f489f
holifieldw <holifield04@gmail.com>
2024-12-20 23:41:05 +0800

    feat: modify constants

--- src/lib/constant/constant.ts
+++ src/lib/constant/constant.ts
@@ -1,7 +1,7 @@
 import { IDropdown } from "../../interface/common";
 
- export const taskStatusOptions: IDropdown[] = [
-    { name: "Done", value: "Done" },
-    { name: "In Progress", value: "In Progress" },
-    { name: "Pending", value: "Pending" },
-  ];
+export const taskStatusOptions: IDropdown[] = [
+  { name: "In Progress", value: "In Progress" },
+  { name: "Done", value: "Done" },
+  { name: "Complete", value: "Complete" },
+];
