28480558d74892ddc4cdb4a1afd8002e363ae966
holifieldw <holifield04@gmail.com>
2024-12-20 18:37:33 +0800

    feat: add router for navigations

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./component/footer/Footer";
 import Sidebar from "./component/sidebar/Sidebar";
-import Homepage from "./page/Homepage";
+import { lazy, Suspense } from "react";
+import Skeleton from "./page/Skeleton";
+
+const Homepage = lazy(() => import("./page/Homepage"));
 
 function App() {
   return (
     <>
       <div className="w-full h-dvh grid grid-cols-7">
-        <Sidebar />
-        <div className="col-span-6 h-full">
-          <header className="w-full h-12 bg-slate-200 border-b border-slate-300 flex items-center p-2">
-            Header
-          </header>
-          <Homepage />
-        </div>
+        <BrowserRouter>
+          <Sidebar />
+          <div className="col-span-6 h-full">
+            <header className="w-full h-12 bg-slate-100 border-b border-slate-300 flex items-center p-2">
+              Task Management
+            </header>
+            <Suspense fallback={<Skeleton />}>
+              <Routes>
+                <Route path="/" element={<Homepage />} />
+              </Routes>
+            </Suspense>
+          </div>
+          <Footer />
+        </BrowserRouter>
       </div>
     </>
   );
