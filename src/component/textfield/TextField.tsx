3076a4371390d2027595f4676d38cb6cb5522b5b
holifieldw <holifield04@gmail.com>
2024-12-20 23:40:19 +0800

    chore: modify textfield structure:

--- src/component/textfield/TextField.tsx
+++ src/component/textfield/TextField.tsx
@@ -7,16 +7,12 @@ export default function TextField(props: TextFieldProps) {
   return (
     <>
       <div className="w-full h-fit flex flex-col gap-1 text-xs">
-        <label htmlFor={props.id}>{props.label}</label>
+        {props.label && <label htmlFor={props.id}>{props.label}</label>}
         <input
-          id={props.id}
-          name={props.id}
-          placeholder={props.placeholder}
-          type={props.type}
-          value={props.value}
-          onChange={props.onChange}
-          disabled={props.disabled}
-          className={`h-8 rounded-md outline-none px-2 border ${props.errors ? "border-red-600" : "border-slate-300"} focus:border-slate-400`}
+          {...props}
+          className={`h-8 rounded-md outline-none px-2 border ${
+            props.errors ? "border-red-600" : "border-slate-300"
+          } focus:border-slate-400`}
         />
         {props.errors && <p className="leading-none text-red-600">Required</p>}
       </div>
